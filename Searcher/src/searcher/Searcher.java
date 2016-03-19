dfa39830c209262eb84876ee0bc7acb6151a90ed
tafmi <teo.moutsos@gmail.com>
2016-03-19 09:20:12 +0200

    Update Searcher.java

--- Searcher/src/searcher/Searcher.java
+++ Searcher/src/searcher/Searcher.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package searcher;
 
 import java.io.File;
@@ -10,17 +6,10 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import org.apache.lucene.queryparser.classic.ParseException;
 
-/**
- *
- * @author Teo
- */
 public class Searcher {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) throws ParseException {
-        // TODO code application logic here
+   
         try{
             if(args.length!=3){
                 throw new InvalidArgumentException();
