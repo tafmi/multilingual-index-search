edfdeeaadb05547ec4895d3eb9112f6952b26eba
tafmi <teo.moutsos@gmail.com>
2016-03-19 09:18:43 +0200

    Update Index.java

--- Searcher/src/searcher/Index.java
+++ Searcher/src/searcher/Index.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package searcher;
 
 import java.io.File;
@@ -29,10 +25,6 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
-/**
- *
- * @author Teo
- */
 public class Index {
     
     private final String lang;
